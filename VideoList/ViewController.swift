85582e7126633222d5431c6fc50ae9899c7fdb9b
sangbeomLee <37286026+sangbeomLee@users.noreply.github.com>
2019-11-29 14:05:00 +0900

    Initial Commit

---
+++ VideoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoList
+//
+//  Created by 이상범 on 29/11/2019.
+//  Copyright © 2019 이상범. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
